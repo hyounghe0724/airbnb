f2c29d4a909f45ace634ca957f3bcca6c8f5e42e
gyuhyeong <hyounghe0724@naver.com>
2022-12-16 14:44:50 +0900

    USER API

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import User
 
 
-class TinyUserSerializer(ModelSerializer):
+class TinyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
             "name",
             "avatar",
             "username",
+        )
+
+
+class PrivateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
         )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound
+from rest_framework.permissions import IsAuthenticated
+from . import serializers
+from users.models import User
 
-# Create your views here.
+
+class Me(APIView):
+
+    permission_classes = [IsAuthenticated]  # private
+
+    def get(self, request):
+
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(
+            user,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")  # password 글자수 제한 구현하기
+        if not password:
+            raise ParseError()
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+# code challange user review see other user
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        except User.DoesNotExist:
+            raise NotFound
+
+
+class ChangePassword(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+    def post(self, request):
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            raise ParseError
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "Wrong password"})
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return  Response({"ok":"bye!"})
